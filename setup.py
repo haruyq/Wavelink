c23ed32e39224952ed52874eeb4ba6e256050b76
haruyq <haruyqhaisinyou@gmail.com>
2025-08-14 17:46:16 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wavelink',
+    version='3.5.1',
+    description='discord.py用のLavalink API Wrapper',
+    author='PythonistaGuild, EvieePy, haruyq',
+    author_email='haruwaiku@gmail.com',
+    url='https://github.com/haruyq/Wavelink',
+    packages=find_packages(),
+    python_requires='>=3.10',
+)
